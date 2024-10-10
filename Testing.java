a593ceb6ed6b8969ac7eb029f88134b2d0b2303e
takhiiiiiiaaaa <935026@pdsb.net>
2024-10-10 14:03:38 -0400

    Create Testing.java

---
+++ Testing.java
@@ -0,0 +1,8 @@
+class Testing {
+  public static void main(String [] args) {
+    // place your code here!
+    String name = "Takhiya";
+    System.out.println("Hello, " + name);
+    // your code ends here!
+  }
+}
