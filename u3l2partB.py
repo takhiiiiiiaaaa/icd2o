76d6dfb60b476117670aff8f3860338548dfb5a0
takhiiiiiiaaaa <935026@pdsb.net>
2024-10-22 14:12:00 -0400

    Create u3l2partB.py

---
+++ u3l2partB.py
@@ -0,0 +1,16 @@
+class Testing {
+  public static void main(String [] args) {
+    // place your code here!
+    int x;
+    x = 12;
+    System.out.println(x);
+    System.out.println("There are " + x + " inches in a foot.");
+    System.out.println("What mammal has the most teeth?");
+    System.out.print("The Giant Armadillo has ");
+    System.out.print(74);
+    System.out.println(" teeth.");
+    System.out.println( "The number of keys on a keyboard is 102.");
+    System.out.println("The value of PI is: 3.14.");
+    // your code ends here!
+  }
+}
