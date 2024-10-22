32df187c43da74e8c78ec8216d32aa49e54bf89c
takhiiiiiiaaaa <935026@pdsb.net>
2024-10-22 14:18:25 -0400

    Create u1l2partc.py

---
+++ u1l2partc.py
@@ -0,0 +1,13 @@
+import java.lang.Math;
+
+class Testing {
+  public static void main(String [] args) {
+    // place your code here!
+   double circ;
+double rad = 3.0; // the radius
+// Math.PI is Java's stored constant for π
+circ = 2 * Math.PI * rad;
+System.out.println(circ);
+    // your code ends here!
+  }
+}
